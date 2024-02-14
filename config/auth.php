1042444cc1921fdfe388b05cb0d2fe32a2bb1f96
Shift <shift@laravelshift.com>
2024-02-14 15:56:31 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/auth.php
+++ config/auth.php
@@ -40,15 +40,18 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
         'api' => [
             'driver' => 'session',
             'provider' => 'users',
             'hash' => true,
         ],
+
         'admin' => [
             'driver' => 'session',
             'provider' => 'admins',
         ],
+
         'branch_staff' => [
             'driver' => 'session',
             'provider' => 'branch_staff',
@@ -82,6 +85,7 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\Admin::class,
         ],
+
         'branch_staff' => [
             'driver' => 'eloquent',
             'model' => App\Models\BranchStaff::class,
@@ -102,9 +106,13 @@ return [
     | than one user table or model in the application and you want to have
     | separate password reset settings based on the specific user types.
     |
-    | The expire time is the number of minutes that each reset token will be
+    | The expiry time is the number of minutes that each reset token will be
     | considered valid. This security feature keeps tokens short-lived so
     | they have less time to be guessed. You may change this as needed.
+    |
+    | The throttle setting is the number of seconds a user must wait before
+    | generating more password reset tokens. This prevents the user from
+    | quickly generating a very large amount of password reset tokens.
     |
     */
 
@@ -115,12 +123,14 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],
+
         'admins' => [
             'provider' => 'admins',
             'table' => 'admin_password_resets',
             'expire' => 60,
             'throttle' => 60,
         ],
+
         'branch_staff' => [
             'provider' => 'branch_staff',
             'table' => 'branch_staff_password_resets',
